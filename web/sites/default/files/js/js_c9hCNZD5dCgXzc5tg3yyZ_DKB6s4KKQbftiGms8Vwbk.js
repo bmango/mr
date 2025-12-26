c0e6c3dc8e7d5b3fc93c5dbd6b3ddbfecbc39b3e
ben mango <ben@benmango.co.uk>
2025-12-26 12:09:20 +0000

    updates

--- web/sites/default/files/js/js_c9hCNZD5dCgXzc5tg3yyZ_DKB6s4KKQbftiGms8Vwbk.js
+++
@@ -1,2 +0,0 @@
-/* @license GPL-2.0-or-later https://www.drupal.org/licensing/faq */
-const theme=localStorage.getItem('theme')||(window.matchMedia('(prefers-color-scheme: dark)').matches?'dark':'light');document.documentElement.setAttribute('data-theme',theme);;
